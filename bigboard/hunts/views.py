75782074980d8d2662210d91e3bd3dd1a1a7febd
Charles Steinhardt <steinhardt@nbi.ku.dk>
2018-12-23 04:36:20 +0100

    Fixed redirects when not logged in (now running in views instead of templates)

--- bigboard/hunts/views.py
+++ bigboard/hunts/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.conf import settings
 from hunts.models import *
 
 # Create your views here.
@@ -6,10 +7,14 @@ def add_hunt(request):
     return render(request, 'hunts/add_hunt.html')
 
 def all_hunts(request):
+    if not request.user.is_authenticated:
+        return redirect('%s?next=%s' % (settings.LOGIN_URL, request.path))
     huntlist = Hunt.objects.raw('SELECT hunts_hunt.* FROM hunts_hunt INNER JOIN hunts_huntuser ON hunts_hunt.huntid = hunts_huntuser.huntid_id WHERE user_id = %s', [request.user.id])
     return render(request, 'hunts/all_hunts.html', {'hunts' : huntlist})
 
 def hunt(request, huntid):
+    if not request.user.is_authenticated:
+        return redirect('%s?next=%s' % (settings.LOGIN_URL, request.path))   
     hunt = Hunt.objects.filter(huntid = huntid)
     roundlist = Round.objects.raw('SELECT COUNT(*), hunts_round.roundid, roundname FROM hunts_puzzle INNER JOIN hunts_puzzround ON hunts_puzzle.puzzid = hunts_puzzround.puzzid_id INNER JOIN hunts_round ON hunts_puzzround.roundid_id = hunts_round.roundid LEFT JOIN hunts_answer ON hunts_answer.puzzid_id = hunts_puzzle.puzzid INNER JOIN hunts_huntuser ON hunts_huntuser.huntid_id = %s WHERE hunts_round.huntid_id = %s AND hunts_round.active = 1 AND hunts_puzzle.active = 1 AND hunts_huntuser.user_id = %s GROUP BY roundid, roundname ORDER BY roundid ASC;', [huntid, huntid, request.user.id])
     puzzlist = []
--- bigboard/puzzle/views.py
+++ bigboard/puzzle/views.py
@@ -1,13 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.conf import settings
 from hunts.models import *
 from puzzle.models import Tag
 
 # Create your views here.
 def add_puzzle(request):
+    if not request.user.is_authenticated:
+        return redirect('%s?next=%s' % (settings.LOGIN_URL, request.path))
     return render(request, 'puzzle/add_puzzle.html')
 
 def puzzle(request, puzzid):    
+    if not request.user.is_authenticated:
+        return redirect('%s?next=%s' % (settings.LOGIN_URL, request.path))
     puzzlist = Puzzle.objects.raw('SELECT hunts_puzzle.*, hunts_hunt.huntname FROM hunts_puzzle INNER JOIN hunts_huntuser ON hunts_huntuser.huntid_id = hunts_puzzle.huntid_id INNER JOIN hunts_hunt ON hunts_hunt.huntid = hunts_puzzle.huntid_id WHERE hunts_puzzle.puzzid = %s AND hunts_huntuser.user_id = %s LIMIT 1', [puzzid, request.user.id])          
-#    print(puzzlist)
     taglist = Tag.objects.filter(puzzid = puzzid)
     return render(request, 'puzzle/onepuzzle.html', {'puzzdata' : puzzlist, 'taglist' : taglist})
