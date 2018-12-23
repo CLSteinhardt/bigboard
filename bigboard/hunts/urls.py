7b651437c734d2fb5bc6dfb6b332de38b9361b7f
Charles Steinhardt <steinhardt@nbi.ku.dk>
2018-12-23 03:28:33 +0100

    Fixed initial page.

--- bigboard/hunts/urls.py
+++ bigboard/hunts/urls.py
@@ -6,7 +6,7 @@ app_name = 'hunts'
 urlpatterns = [
     re_path(r'^hunts/add/$', views.add_hunt, name = 'add'),
     re_path(r'^hunts/$', views.all_hunts, name = 'all'),
-    re_path(r'^/$', views.all_hunts, name = 'all'),
+    re_path(r'^$', views.all_hunts, name = 'all'),
     re_path(r'^hunt/(?P<huntid>\w+)/$', views.hunt, name = 'hunt'),
     re_path(r'^', include('puzzle.urls')),
 ]
